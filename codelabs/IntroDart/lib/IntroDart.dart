5a9f4d7aef4118828ce53a416d29228e6d5c4912
ysera34 <ycyoon34@gmail.com>
2019-02-16 17:21:35 +0900

    codelab intro to dart: 2. Create a simple Dart class

--- codelabs/IntroDart/lib/IntroDart.dart
+++ codelabs/IntroDart/lib/IntroDart.dart
@@ -1,3 +1,46 @@
+
+
 int calculate() {
   return 6 * 7;
+}
+
+class Bicycle {
+  int cadence;
+  int _speed = 0;
+  int get speed => _speed;
+  int gear;
+
+  Bicycle(this.cadence, this.gear);
+
+  /**
+   * Bicycle(int cadence, int speed, int gear) {
+   *   this.cadence = cadence;
+   *   this.speed = speed;
+   *   this.gear = gear;
+   * }
+   * Bicycle(this.cadence, this.speed, this.gear);
+   */
+
+  void applyBrake(int decrement) {
+    _speed -= decrement;
+  }
+
+  void speedUp(int increment) {
+    _speed += increment;
+  }
+
+  /**
+   * @override
+   * String toString() {
+   *   'Bicycle: $speed mph';
+   * }
+   */
+  @override
+  String toString() => 'Bicycle: $speed mph';
+}
+
+main(List<String> arguments) {
+  print('Hello world: ${calculate()}!');
+  var bike = new Bicycle(2, 1);
+  print(bike);
 }
--- codelabs/IntroDart/test/IntroDart_test.dart
+++ codelabs/IntroDart/test/IntroDart_test.dart
@@ -6,14 +6,3 @@ void main() {
     expect(calculate(), 42);
   });
 }
-
-class Bicycle {
-  int cadence;
-  int speed;
-  int gear;
-
-  Bicycle(this.cadence, this.speed, this.gear);
-
-
-}
-
