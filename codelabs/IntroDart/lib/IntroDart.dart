913a64e933ce6a92bacc368d3875d23228ba5dfa
ysera34 <ycyoon34@gmail.com>
2019-02-17 18:51:02 +0900

    codelab intro to dart: 6. Use Dart for functional programming

--- codelabs/IntroDart/lib/IntroDart.dart
+++ codelabs/IntroDart/lib/IntroDart.dart
@@ -94,6 +94,8 @@ Shape shapeFactory(String type) {
   throw 'Can\'t create $type.';
 }
 
+String scream(int length) => "A${'a' * length}h!";
+
 main(List<String> arguments) {
   print('Hello world: ${calculate()}!');
   var bike = new Bicycle(2, 1);
@@ -118,4 +120,13 @@ main(List<String> arguments) {
   } catch (err) {
     print(err);
   }
+
+  final values =  [1, 2, 3, 5, 10, 50];
+  for (var length in values) {
+    print(scream(length));
+  }
+  // Convert imperative code to functional
+  values.map(scream).forEach(print);
+
+  values.skip(1).take(3).map(scream).forEach(print);
 }
