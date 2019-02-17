e8dabb48ac38c2c14753d78c067c161cacdc5141
ysera34 <ycyoon34@gmail.com>
2019-02-17 18:38:55 +0900

    codelab intro to dart: 5. Implement an interface

--- codelabs/IntroDart/lib/IntroDart.dart
+++ codelabs/IntroDart/lib/IntroDart.dart
@@ -72,6 +72,11 @@ class Circle implements Shape {
   num get area => pi * pow(radius, 2);
 }
 
+class CircleMock implements Circle {
+  num radius;
+  num area;
+}
+
 class Square implements Shape {
   final num side;
   Square(this.side);
