24c11482e443911d7831786118aee07cd8d229d7
ysera34 <ycyoon34@gmail.com>
2019-02-19 00:45:10 +0900

    codelab first flutter app: 5. Add icons to the list

--- codelabs/first_flutter_app/lib/main.dart
+++ codelabs/first_flutter_app/lib/main.dart
@@ -34,6 +34,7 @@ class MyApp extends StatelessWidget {
 
 class RandomWordsState extends State<RandomWords> {
   final List<WordPair> _suggestions = <WordPair>[];
+  final Set<WordPair> _saved = new Set<WordPair>();
   final TextStyle _biggerFont = const TextStyle(fontSize: 20.0);
 
   @override
@@ -83,11 +84,16 @@ class RandomWordsState extends State<RandomWords> {
   }
 
   Widget _buildRow(WordPair pair) {
+    final bool alreadySaved = _saved.contains(pair);
     return new ListTile(
       title: new Text(
         pair.asPascalCase,
         style: _biggerFont,
-      )
+      ),
+      trailing: new Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
     );
   }
 }
