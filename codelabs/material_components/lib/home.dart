c47eac87963203a7e9a51060dd66b12576d8582b
ysera34 <ycyoon34@gmail.com>
2019-03-02 19:33:18 +0900

    codelab MDC-102: 1. Add a top app bar

--- codelabs/material_components/lib/home.dart
+++ codelabs/material_components/lib/home.dart
@@ -23,6 +23,39 @@ class HomePage extends StatelessWidget {
     // TODO: Pass Category variable to AsymmetricView (104)
     return Scaffold(
       // TODO: Add app bar (102)
+      appBar: AppBar(
+        // TODO: Add Buttons and title (102)
+        leading: IconButton(
+          icon: Icon(
+            Icons.menu,
+            semanticLabel: 'menu',
+          ),
+          onPressed: () {
+            print('Menu button');
+          },
+        ),
+        title: Text('SHRINE'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(
+              Icons.search,
+              semanticLabel: 'search',
+            ),
+            onPressed: () {
+              print('Search button');
+            },
+          ),
+          IconButton(
+            icon: Icon(
+              Icons.tune,
+              semanticLabel: 'filter',
+            ),
+            onPressed: () {
+              print('Filter button');
+            },
+          ),
+        ],
+      ),
       // TODO: Add a grid view (102)
       body: Center(
         child: Text('You did it!'),
